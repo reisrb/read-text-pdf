e6ce5798a2dbe8bd1398e9ba42a1ff372d6089fe
CeloAugusto - CLI <marcelo.santos@bandtec.com.br>
2021-08-06 18:16:37 -0300

    feat: Get the last columns

--- model/pdf.py
+++ model/pdf.py
@@ -1,16 +1,30 @@
-from dataclasses import dataclass, asdict
+from dataclasses import asdict, dataclass
+from typing import List
+from copy import deepcopy
+
+from pandas import DataFrame
+
 from model.columns_pdf import ColumnsPdf
+
 
 @dataclass
 class Pdf:
-    n_instalacao: str = ''
-    n_cliente: str = ''
-    n_medidor: str = ''
-    cnpj: str = ''
-    endereco: str = ''
-    cep: str = ''
+    n_instalacao: str = ""
+    n_cliente: str = ""
+    n_medidor: str = ""
+    cnpj: str = ""
+    endereco: str = ""
+    cep: str = ""
 
-    tabela: list = ''
+    tabela: List[ColumnsPdf] = ""
 
-    def to_string(self) -> dict:
+    def to_dict(self) -> dict:
         return asdict(self)
+
+    def to_dataframe(self) -> DataFrame:
+        tmp = deepcopy(self)
+        tmp.tabela = [f"0...{len(self.tabela)}"]
+        return DataFrame(tmp.to_dict())
+
+    def tabela_to_dataframe(self) -> DataFrame:
+        return DataFrame([i.to_dict() for i in self.tabela])
